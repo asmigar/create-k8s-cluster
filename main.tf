855f6c0d59822fd72188902caefe370bd91a7c7e
Sagar Maurya <15274533+sagarising@users.noreply.github.com>
2023-07-29 08:48:29 +0530

    create ssh key

--- main.tf
+++ main.tf
@@ -53,9 +53,25 @@ resource "aws_security_group" "allow_ssh" {
   }
 }
 
-resource "aws_key_pair" "webserver" {
-  key_name   = "webserver"
-  public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQDMK0sHujc2rfg3y4blCK46ye7jMTmDa3jXqGVL68ts87zo95JZdQakLQlLiUkVvm7IYOePctjUZDRtaUgnWPWdC/QYQvLLiAkO/IzgcHIyvURNjpVd94I67afYygzIeagsKQJW+igVeztw95fkwW8gvb2DirM4UkOEN6FnGMjBDOFjTJSu1AKqgzr7cqV7JMdVqDLlODGS6Q++H/xv2WTFNXNBR9NFTtTrxJl5+iFKrtg8D/T+1/5LvUwclDd9un9Jb3SYCtxwRK00I3E1nNSviT1zxsUrtkSMt8B+qngwUGjT5whIdpV3109qFt9F4/lEbcrpTtuoiH/nCgQYNL7CZMKVn12r0jg/ADOHpqj56sxh81Vx/NWRi4t1H8d7OZ/aQ3pFMtW1fB+kcdvwT1fNJRAW1U1m6rR2yd6QjUR+5jiBLfvpH6PIwdn1OpVPGRtAUIQxkOeRkTeUsAbvBm2PvWSnoAtgLv/8GzlzY00uw0odLlj/RhJlIHYkyXqETTs= sagarmaurya@sagar.local"
+
+
+resource "tls_private_key" "this" {
+  algorithm = "RSA"
+  rsa_bits  = 4096
+
+  provisioner "local-exec" {
+    command = "echo '${self.private_key_openssh}' > ~/.ssh/${var.shh_key_name}.pem; chmod 400 ~/.ssh/docker_swarm.pem"
+  }
+
+  provisioner "local-exec" {
+    when = destroy
+    command = "rm -rf ~/.ssh/${var.shh_key_name}.pem"
+  }
+}
+
+resource "aws_key_pair" "this" {
+  key_name   = var.shh_key_name
+  public_key = tls_private_key.this.public_key_openssh
 }
 
 resource "aws_instance" "master" {
@@ -68,7 +84,7 @@ resource "aws_instance" "master" {
 
   vpc_security_group_ids = [aws_security_group.allow_ssh.id]
   subnet_id              = aws_subnet.public.id
-  key_name               = aws_key_pair.webserver.key_name
+  key_name               = aws_key_pair.this.key_name
   user_data              = <<-EOT
 		#!/bin/bash
 		EOT
@@ -85,7 +101,7 @@ resource "aws_instance" "worker" {
 
   vpc_security_group_ids = [aws_security_group.allow_ssh.id]
   subnet_id              = aws_subnet.public.id
-  key_name               = aws_key_pair.webserver.key_name
+  key_name               = aws_key_pair.this.key_name
   user_data              = <<-EOT
 		#!/bin/bash
 		EOT
--- vars.tf
+++ vars.tf
@@ -8,3 +8,8 @@ variable "public_subnet_cidr_block" {
   default = "10.0.1.0/24"
 }
 
+variable "shh_key_name" {
+  type = string
+  default = k8s
+  description = "ssh key name to be created in EC2 and store in ~/.ssh folder"
+}
