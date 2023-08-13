05d52d9817477ea35dcf47f489c419334184117d
Sagar Maurya <15274533+sagarising@users.noreply.github.com>
2023-08-13 16:18:17 +0530

    fix tf formatting

--- main.tf
+++ main.tf
@@ -19,11 +19,11 @@ resource "aws_security_group" "allow_ssh" {
   vpc_id      = aws_vpc.main.id
 
   ingress {
-    description      = "ssh"
-    from_port        = 22
-    to_port          = 22
-    protocol         = "tcp"
-    cidr_blocks      = ["${data.http.my_public_ip.response_body}/32"]
+    description = "ssh"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["${data.http.my_public_ip.response_body}/32"]
   }
 
   ingress {
